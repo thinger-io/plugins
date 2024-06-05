1d45f328022d01daed4fc4f6092587640c0713f5
Jaime Bautista <jaimebs94@gmail.com>
2024-06-05 16:36:18 +0200

    Bumped code-server version and updated extensions

--- vscode/CHANGELOG.md
+++ vscode/CHANGELOG.md
@@ -1,28 +1,45 @@
 
 # Changelog
 
-## 4.14.1-2 - 2023-08-23
+## 4.89.1-1 - 2024-06-05
+
 ### Changed
+
+- Bumped to code-server version 4.89.1
+- Bumped platformio extension to 3.3.3
+- Bumped thinger.io extension to 1.1.0
+
+## 4.14.1-2 - 2023-08-23
+
+### Changed
+
 - Marginally reduce the limit of memory to be able to run
 
 ## 4.14.1-1 - 2023-07-26
+
 ### Added
+
 - Preinstallation of platformio-ide and thinger-io extension
 - Default settings for Thinger.io extension configuration
 
 ### Changed
+
 - Bumped to code-server version 4.14.1
 
 ## 4.11.0-3 - 2023-03-31
+
 - Updated deprecated mod for code-server
 
 ## 4.11.0-2 - 2023-03-29
+
 - Updated mount points for storages
 
 ## 4.11.0-1 - 2023-03-28
+
 - Bumped code-server to version 4.11.0
 
 ## 4.6.0-1 - 2022-08-26
+
 - Downsized resource memory to 3.75GB
 - Bumped to code-server version 4.6.0
 
--- vscode/scripts/extensions.sh
+++ vscode/scripts/extensions.sh
@@ -8,21 +8,21 @@ sudo apt install -yqq -o=Dpkg::Use-Pty=0 gettext python3-venv > /dev/null
 EXTENSIONS=`code-server --list-extensions --show-versions`
 
 # Install cpptools-linux extension
-if ! echo "$EXTENSIONS" | grep -q "cpptools@1.16.3" > /dev/null; then
+if ! echo "$EXTENSIONS" | grep -q "cpptools@1.20.5" > /dev/null; then
     echo "Installing cpptools"
-    curl -s -L -o cpptools-linux.vsix https://s3.eu-west-1.amazonaws.com/thinger.io.files/plugins/vscode/vsix/ms-vscode.cpptools-1.16.3%40linux-x64.vsix 
+    curl -s -L -o cpptools-linux.vsix https://s3.eu-west-1.amazonaws.com/thinger.io.files/plugins/vscode/vsix/ms-vscode.cpptools-1.20.5%40linux-x64.vsix
     code-server --install-extension cpptools-linux.vsix
 fi
 
 # Install platformio extension
-if ! echo "$EXTENSIONS" | grep -q "platformio-ide@3.3.0" > /dev/null; then
-    curl -s -L -o platformio-ide.vsix https://s3.eu-west-1.amazonaws.com/thinger.io.files/plugins/vscode/vsix/platformio.platformio-ide-3.3.0%40linux-x64.vsix
+if ! echo "$EXTENSIONS" | grep -q "platformio-ide@3.3.3" > /dev/null; then
+    curl -s -L -o platformio-ide.vsix https://s3.eu-west-1.amazonaws.com/thinger.io.files/plugins/vscode/vsix/platformio.platformio-ide-3.3.3%40linux-x64.vsix
     code-server --install-extension platformio-ide.vsix
 fi
 
 # Install thinger-io extension
-if ! echo "$EXTENSIONS" | grep -q "thinger-io@1.0.6" > /dev/null; then
-    curl -s -L -o thinger-io.vsix https://s3.eu-west-1.amazonaws.com/thinger.io.files/plugins/vscode/vsix/thinger-io.thinger-io-1.0.6.vsix
+if ! echo "$EXTENSIONS" | grep -q "thinger-io@1.1.0" > /dev/null; then
+    curl -s -L -o thinger-io.vsix https://s3.eu-west-1.amazonaws.com/thinger.io.files/plugins/vscode/vsix/thinger-io-1.1.0.vsix
     code-server --install-extension thinger-io.vsix
 fi
 
