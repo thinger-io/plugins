f0ca11c696668e6118476804f172bbd2815648bb
seniorbeto <100471939@alumnos.uc3m.es>
2025-06-30 11:44:01 +0200

    token secttion in settings

--- ttn/task/frontend/src/app/features/settings/settings.component.ts
+++ ttn/task/frontend/src/app/features/settings/settings.component.ts
@@ -2,10 +2,13 @@ import { Component } from '@angular/core';
 import { NzCardComponent } from "ng-zorro-antd/card";
 import { AppConfigService } from "../../core/services/app-config.service";
 import { NzSkeletonComponent } from "ng-zorro-antd/skeleton";
-import { faEye, faEyeSlash, faGears, faInfoCircle } from "@fortawesome/free-solid-svg-icons";
-import { faQuestionCircle } from "@fortawesome/free-regular-svg-icons"
+import { faEye, faEyeSlash, faGears, faInfoCircle, faUpRightAndDownLeftFromCenter } from "@fortawesome/free-solid-svg-icons";
+import { faQuestionCircle, faCopy } from "@fortawesome/free-regular-svg-icons"
 import { FaIconComponent } from "@fortawesome/angular-fontawesome";
 import { NzTooltipDirective } from "ng-zorro-antd/tooltip";
+import { NzModalService } from 'ng-zorro-antd/modal';
+import { NzMessageService } from 'ng-zorro-antd/message';
+import { Clipboard } from '@angular/cdk/clipboard';
 
 @Component({
     selector: 'app-settings',
@@ -24,14 +27,59 @@ export class SettingsComponent {
   protected faGears = faGears;
   protected faQuestionCircle = faQuestionCircle;
   protected faInfoCircle = faInfoCircle;
+  protected faCopy = faCopy;
+  protected faUpRightAndDownLeftFromCenter = faUpRightAndDownLeftFromCenter;
 
   protected endpointUrl: string;
-
   protected tokenVisible = false;
+  protected ttnToken = '';
 
-  constructor( private appConfigService: AppConfigService ) {
+  constructor(
+    private appConfigService: AppConfigService,
+    private modal: NzModalService,
+    private clipboard: Clipboard,
+    private message: NzMessageService
+  ) {
     const config = this.appConfigService.getConfig();
     this.endpointUrl = window.location.origin + window.location.pathname;
+
+    // RECUPERA EL TOKEN DEL BACKEND (/env)
+    fetch('/env')
+      .then(resp => resp.json())
+      .then(env => this.ttnToken = env['THINGER_TOKEN_TTN_PLUGIN'] ?? '');
   }
 
+  showTokenModal(): void {
+    this.modal.create({
+      nzTitle: 'TTN API Token',
+      nzContent: `
+        <div style="width: 520px; word-break: break-all; padding: 18px 0;">
+          <span style="color: #888;">Bearer </span>
+          <span style="font-family: monospace; user-select: all;">${this.ttnToken}</span>
+        </div>
+      `,
+      nzFooter: [
+        {
+          label: 'Copy',
+          type: 'primary',
+          onClick: () => {
+            this.copyToken();
+            this.modal.closeAll();
+          }
+        },
+        {
+          label: 'Close',
+          onClick: () => this.modal.closeAll()
+        }
+      ],
+      nzClosable: true,
+      nzMaskClosable: true,
+      nzBodyStyle: { padding: '0 24px' }
+    });
+  }
+
+  copyToken(): void {
+    this.clipboard.copy(`Bearer ${this.ttnToken}`);
+    this.message.success('Copied!');
+  }
 }
