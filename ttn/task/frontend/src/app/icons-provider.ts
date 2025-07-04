3ab7699c925f545a29bf73f5f44643f8475de88a
Alberto <albertopenasdiaz@gmail.com>
2025-07-04 11:11:05 +0200

    Clean Dragino PLugin Branch

--- ttn/task/frontend/src/app/icons-provider.ts
+++
@@ -1,8 +0,0 @@
-import {
-  MenuFoldOutline,
-  MenuUnfoldOutline,
-  FormOutline,
-  DashboardOutline
-} from '@ant-design/icons-angular/icons';
-
-export const icons = [MenuFoldOutline, MenuUnfoldOutline, DashboardOutline, FormOutline];
